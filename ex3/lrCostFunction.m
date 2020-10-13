1d91a8e1610469d488b09dab42a776e7c266c22d
Wu Wenhan <1020645987@qq.com>
2020-10-13 21:38:09 +0800

    Ex.3 completed

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,7 +36,10 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
+theta_grad=theta
+theta_grad(1)=0
+J=1/m*sum(-y.*log(sigmoid(X*theta))-(1-y).*(log(1-sigmoid(X*theta))))+lambda/2/m*(theta_grad.'*theta_grad)
+grad = 1/m*X.'*(sigmoid(X*theta)-y)+lambda/m*theta_grad
 
 
 
